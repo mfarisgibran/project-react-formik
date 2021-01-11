e487c33e31291579267c8d5b131d7bd570ed9858
M Faris Gibran <mfarisgibran@gmail.com>
2021-01-11 20:32:19 +0700

    Display form values

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+import { useState } from 'react'
 import { Formik, Field, Form } from 'formik'
 
 export default function App() {
+  const [form, setForm] = useState({})
+
   return (
     <div>
       <h1>Contact Me</h1>
       <Formik
-        initialValues={{ name: '' }}
+        initialValues={{
+          name: '',
+          email: '',
+        }}
         onSubmit={(values) => {
-          console.log(values)
+          setForm(values)
         }}
       >
         <Form>
           <Field type="text" name="name" />
+          <Field type="email" name="email" />
           <button type="submit">Submit</button>
         </Form>
       </Formik>
+      <div>
+        <ul>
+          <li>Name: {form.name}</li>
+          <li>Email: {form.email}</li>
+        </ul>
+      </div>
     </div>
   )
 }
